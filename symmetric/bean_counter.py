d6ce9997f46cfc9b009c4a7bbf2238b6040095a3
ryo_mydesk <9rira.1325@gmial.com>
2023-08-07 02:04:57 +0900

    add symmtric

--- symmetric/bean_counter.py
+++ symmetric/bean_counter.py
@@ -1,11 +1,18 @@
 import requests
-
+from pwn import xor
 base_url = "https://aes.cryptohack.org/bean_counter/"
 response = requests.get(url="%s/encrypt/" % (base_url)).json()
 word_list = list(range(ord('a'),ord('z')+1)) + list(range(ord('0'),ord('9')+1)) +[ord("{"),ord("}")]
 print(word_list)
 # print(response)
-# ciphertext = response['encrypted']
+#pngであることを示すpngsignature + pngの画像の情報を記すihdrという情報に分かれている
+png_header = "89504e470d0a1a0a" + "0000000d49484452" 
+ciphertext = bytes.fromhex(response['encrypted'])
+
+key = xor(bytes.fromhex(png_header),(ciphertext[:16]))
+with open('./flag.png','wb')as f:
+   f.write(xor(ciphertext,key))
+# print(png_header)
 # print(len(ciphertext) )
 # print(len(ciphertext) % 32)
 
