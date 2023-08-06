32f857989ef53c5fee3f5ca85eb0e4715b3dec8f
ryo_mydesk <9rira.1325@gmial.com>
2023-08-07 04:12:20 +0900

    add paramater_injection

---
+++ pub/paramater_injection.py
@@ -0,0 +1,32 @@
+from netcat import Netcat
+import json
+# start a new Netcat() instance
+nc = Netcat('socket.cryptohack.org', 13371)
+key_list = []
+
+ # receive lines, print and solve fomula
+for i in range(2):
+    lines = nc.read().decode('utf-8').split('\n')
+    print(lines)
+Alice_dict = lines[0]
+# print(Alice_dict)
+#send_to_bob
+nc.write(Alice_dict)
+print('-'*6)
+for i in range(2):
+    lines = nc.read().decode('utf-8').split('\n')
+    print(lines)
+Bob_dict = lines[0]
+Bob_fake_dict = '{"B": "0x01"}'
+
+#send_to_alice
+nc.write(Bob_fake_dict)
+print('-'*6)
+# recieve from _ alice 
+for i in range(3):
+    lines = nc.read().decode('utf-8').split('\n')
+    print(lines)
+    
+
+
+     
